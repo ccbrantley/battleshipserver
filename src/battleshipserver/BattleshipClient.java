e8bb318c37b40738dd0e3a92f5b1869fe5737495
ccbrantley <ccbrantley@gtcc.edu>
2019-10-20 04:52:24 -0400

    Updated Style

--- src/battleshipserver/BattleshipClient.java
+++ src/battleshipserver/BattleshipClient.java
@@ -10,32 +10,28 @@ public class BattleshipClient {
         newString[0] = "###.###.#.##";
         newString[1] = "23";
         if (newString.length != 2) {
-            System.err.println(
-                "Usage: Battleship <23>");
+            System.err.println("Usage: Battleship <23>");
             System.exit(1);
         }
         String hostName = newString[0];
         int portNumber = Integer.parseInt(newString[1]);
-     try (
-            Socket clientSocket = new Socket(hostName, portNumber);
-            DataOutputStream out =
-                new DataOutputStream(clientSocket.getOutputStream());
-            DataInputStream in =
-                new DataInputStream(clientSocket.getInputStream());
-            BufferedReader stdIn =
-                new BufferedReader(
-                    new InputStreamReader(System.in))
-        ) {
-         new Thread(new InputThread(in)).start();
-         String tempString;
-         do {
-             if(((tempString) = stdIn.readLine()) != null) {
-                 out.writeUTF(tempString);
-             }
-         } while (true);
-     } catch (Exception e) {
-         System.err.println(e.toString());
-         System.exit(1);
-     }
+        try (
+                Socket clientSocket = new Socket(hostName, portNumber);
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+                )
+        {
+            new Thread(new InputThread(in)).start();
+            String tempString;
+            do {
+                if(((tempString) = stdIn.readLine()) != null) {
+                    out.writeUTF(tempString);
+                }
+            } while (true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
     }
 }
